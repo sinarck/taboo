872c9c71cec47738ca5f0c96ecc93c88539b2877
Aadi <aadisang@outlook.com>
2026-05-14 03:53:14 -0500

    feat: scaffold tanstack start with vite + nitro

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: "src",
+      router: {
+        routesDirectory: "routes",
+      },
+    }),
+    react(),
+    nitro(),
+  ],
+})
